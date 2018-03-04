a5827308902632122af6715769f1243d56fc286d
swedendrift <swedendrift@gmail.com>
2018-03-03 17:30:31 -0800

    upgrading versions

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from '../components/myLayout.js'
+
+export default () => (
+    <Layout>
+       <p>This is the about page</p>
+    </Layout>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,54 @@
-const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-  
-  export default Index
+import Layout from '../components/myLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Helvetica";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </Layout>
+)
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
